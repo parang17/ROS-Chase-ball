da11598283aed00371049ea82c7609a9681dbbee
youngjun <parang17@gmail.com>
2020-05-15 01:50:49 -0400

    init update

---
+++ catkin_ws/src/ball_chaser/cmake-build-debug/CMakeFiles/drive_bot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drive_bot.dir/src/drive_bot.cpp.o"
+  "devel/lib/ball_chaser/drive_bot"
+  "devel/lib/ball_chaser/drive_bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drive_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
